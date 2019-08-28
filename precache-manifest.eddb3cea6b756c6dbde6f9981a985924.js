263e7c620e27c55ebe65d6fe230f0c13cfce3869
buresj <bures@fakultnisociologie.cz>
2019-08-28 21:07:49 +0200

    Updates

---
+++ precache-manifest.eddb3cea6b756c6dbde6f9981a985924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b825eba7a7f9c3f1447215fb8f202234",
+    "url": "/HiDoggie/index.html"
+  },
+  {
+    "revision": "a647dbc93b282b24e86f",
+    "url": "/HiDoggie/static/js/2.347839a1.chunk.js"
+  },
+  {
+    "revision": "b769bb5e4db6dfe53698",
+    "url": "/HiDoggie/static/js/main.db004da3.chunk.js"
+  },
+  {
+    "revision": "a870f1870f4e9fc67acd",
+    "url": "/HiDoggie/static/js/runtime~main.269641a3.js"
+  },
+  {
+    "revision": "0014511f9d56bd1218a057cf7ee82efc",
+    "url": "/HiDoggie/static/media/5554.0014511f.jpg"
+  },
+  {
+    "revision": "e81b77e66f8b18f34d188912cd9bcc56",
+    "url": "/HiDoggie/static/media/bruno_avatar.e81b77e6.png"
+  },
+  {
+    "revision": "47ae5de74976556a54a68ff5936b7d9a",
+    "url": "/HiDoggie/static/media/logo.47ae5de7.png"
+  }
+]);
